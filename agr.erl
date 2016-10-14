c7d6bbb2bb2819de7fa2713c428f2248dff2013f
wudanyang <w00376947@china.huawei.com>
2016-10-14 17:13:04 +0800

    yo, weekend

--- agr.erl
+++ agr.erl
@@ -1,5 +1,5 @@
 -module(agr).
--export([init/1, start/0, upload/3,query/0,test/0]).
+-export([init/1, start/0, upload/3,query/0,cims/3]).
 
 -record(agr_info, {agr_id, cer_id, name, card_id,status,  county, inst, time}).
 -record(cer_info, {cer_id, name, status, time}).
@@ -21,21 +21,32 @@ init(From) ->
 			ok
 	end.
 upload(AgrId, CerId, Name) ->
-	Tmp = #agr_info{agr_id = AgrId, cer_id = CerId, name = Name},
-	mnesia:dirty_write(Tmp).
+	Ori = mnesia:dirty_read(agr_info, AgrId),
+	case Ori of
+		[] ->			
+			Tmp = #agr_info{agr_id = AgrId, cer_id = CerId, name = Name},
+			mnesia:dirty_write(Tmp),
+			ok;
+		_ ->
+			existed
+	end.
+cims(MyPid, CerId, Name) ->
+	MyPid ! {role, CerId, cer_ok}.
 query() ->
 	query(agr_info).
 query(Tab) ->
 	lists:flatmap(  
 		fun(Key) ->  
 			[Result] = mnesia:dirty_read(Tab, Key),
+			"rest," ++
 			lists:flatmap(fun(C) ->
 							case C of
 								agr_info -> [];
 								undefined -> [","];								
 								_ -> [binary_to_list(C),","]
 							end
-			             end, tuple_to_list(Result)) ++ "\n"
+			             end, tuple_to_list(Result)) ++ 
+			"\n"
 		end, mnesia:dirty_all_keys(Tab)).  
 test() ->
 	List = ["ABC"],
--- role.erl
+++ role.erl
@@ -7,6 +7,8 @@ login(From) ->
 	receive
 		{From, data, <<"login",End/binary>>} ->
 			From ! {self(), data, "login success."},
+			ets:new(cer_agr, [set, named_table]),
+			ets:new(agr_cer, [set, named_table]),
 			loop(From);
 		{From, data, _} ->
 			login(From);
@@ -24,13 +26,35 @@ loop(From) ->
 		{From, data, <<"query:", CerId:18/binary>>} ->
 			loop(From);
 		{From, data, <<"upload:",AgrId:13/binary,",",CerId:18/binary, ",", Name/binary>>} ->
-			Ret = agr:upload(AgrId, CerId, Name),
-			From ! {self(), data, <<"resp,",AgrId/binary,",good">>},
+			ets:insert(agr_cer, {AgrId, CerId, Name}),
+			ets:insert(cer_agr, {CerId, AgrId}),
+			spawn(agr, cims, [self(), CerId, Name]),			
+			From ! {self(), data, <<"resp,",AgrId/binary,",checking...">>},
+			loop(From);
+		{role, CerId, Ret} ->
+			[{_, AgrId}] = ets:take(cer_agr, CerId),
+			[{_, _, Name}] = ets:take(agr_cer, AgrId),
+			case Ret of
+				cer_id_null ->
+					From ! {self(), data, <<"resp,", AgrId/binary, ",cer_id_null">>};
+				cer_id_dismatch ->
+					From ! {self(), data, <<"resp,", AgrId/binary, ",cer_id_dismatch">>};					
+				cer_ok ->
+					From ! {self(), data, <<"resp,", AgrId/binary, ",cer_ok">>},
+					case agr:upload(AgrId, CerId, Name) of
+						ok ->
+							From ! {self(), data, <<"resp,", AgrId/binary, ",uploaded">>};
+						existed ->
+							From ! {self(), data, <<"resp,", AgrId/binary, ",already existed">>}
+					end
+			end,
 			loop(From);
 		{From, data, Unknown} ->
-			io:format("role.unknon~w~n", [Unknown]),
-			loop(From)
+			io:format("role.get~w~n", [Unknown]),
+			loop(From);
+		_Any ->
+			io:format("role.unknown:~p~n", _Any)
 	end.
-	
-
+cims() ->
+	ok.
 	
